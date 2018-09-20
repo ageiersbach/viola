24ea630e0ef2635efa06b4eda3d1b6e5bb6ec162
Anne Geiersbach <annasazi@gmail.com>
2018-09-20 17:15:57 -0400

    @wip

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,13 @@ extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 extern crate chrono;
+extern crate rusqlite;
+use rusqlite::Connection;
 
 mod journal;
 
 fn main() {
-    println!("Hello, world!");
+    let connection = Connection::open("../db.sqlite3").unwrap();
+    let stmt = connection.execute("SELECT * FROM journals;", &[]).unwrap();
+    println!("Hello, world! ${}", &stmt);
 }
